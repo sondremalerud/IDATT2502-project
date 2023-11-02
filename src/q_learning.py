6baaa24dafcf34fad619f6883b9392f035e6ed19
Sondre Malerud <sondre_mal@hotmail.no>
2023-11-02 12:38:27 +0100

    Fixed exp_rate parameters

--- src/q_learning.py
+++ src/q_learning.py
@@ -4,18 +4,17 @@ from nes_py.wrappers import JoypadSpace
 import mario_bros_env
 from mario_bros_env.actions import RIGHT_ONLY
 from filters import grayscale, downscale
-import math
 
 class Model:
     def __init__(self, env):
         self.states = []
         self.actions = actions.RIGHT_ONLY
         self.lr = 0.2
-        self.exp_rate = 0.1
+        self.exp_rate = 1.0
         self.env = env
         self.discount = 0.99
         self.observation_space = self.env.observation_space.shape
-        #self.Q_Values = np.zeros(self.observation_space + (self.env.action_space.n,))
+        self.decay_rate = 0.995
         self.Q_Values = np.zeros((256, 240, self.env.action_space.n))
 
     def action(self, state):
@@ -27,8 +26,8 @@ class Model:
             action = np.argmax(self.Q_Values[state])
         return action
     
-    def get_exploration_rate(self, episode):
-        return max(0.1, min(1., 1. - math.log10((episode + 1) / 0.0001)))
+    def get_exploration_rate(self):
+        return self.exp_rate * self.decay_rate
     
     def updated_q_value(self, state, action, reward, new_state):
         return (self.lr * (reward + self.discount * np.max(self.Q_Values[new_state]) - self.Q_Values[state][action]))
@@ -38,28 +37,28 @@ class Model:
         rescaled_state = downscale.divide(gray_state, 9)
         return rescaled_state
 
-    def train(self, rounds=10, steps=2000):
+    def train(self, episodes=100, steps=1000):
         rewards = []
-        for episode in range(rounds):
-            self.exp_rate = self.get_exploration_rate(episode)
+        for episode in range(episodes):
+            self.exp_rate = self.get_exploration_rate()
             observation, info = self.env.reset()
             state = self.processFrame(observation)
             done = False
-            reward_current_ep = 0 
-            print("Episode "+str(episode))
+            reward_current_ep = 0
             for step in range(steps):
                 self.env.render()
-                print("Step: "+str(step))
                 action = self.action(state)
                 new_state, reward, done, truncated, info = self.env.step(action)
                 new_state = self.processFrame(new_state)
                 self.Q_Values[new_state][action] += self.updated_q_value(state, action, reward, new_state)
                 state = new_state
-                reward_current_ep += 1
+                reward_current_ep += reward
                 if done:
                     break
             rewards.append(reward_current_ep)
             print(f"Score for episode {episode+1}: {rewards[episode]}")
+            print(f"Exploration probability: {self.exp_rate}")
+
 
         print('Finished')
         np.save("q_values.npy", self.Q_Values)
@@ -82,10 +81,10 @@ class Model:
 
 env = mario_bros_env.make(
     'SuperMarioBros-v0',
-    render_mode="human"
+    render_mode=None
 )
 env = JoypadSpace(env, RIGHT_ONLY)
 
 model = Model(env)
 model.train()
-#model.run()
+model.run()
