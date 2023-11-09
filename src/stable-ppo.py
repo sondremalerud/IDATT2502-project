fd8d2472595e95404ba6b55f2a5c651a15c93804
Katarzyna Szlejter <kasia.szlejter@gmail.com>
2023-11-09 11:46:07 +0100

    Fixed imports

--- src/stable-ppo.py
+++ src/stable-ppo.py
@@ -3,15 +3,16 @@ from nes_py.wrappers import JoypadSpace
 from mario_bros_env.actions import SIMPLE_MOVEMENT
 from stable_baselines3 import PPO
 from stable_baselines3.common.vec_env import DummyVecEnv, VecFrameStack
-import wrappers
+from wrappers import GrayscaleEnv, DownsampledEnv, DownscaledEnv
+from wrappers.filters import GrayscaleFilters
 # tjuvlånt fra https://github.com/BJEnrik/reinforcement-learning-super-mario
 
 env = mario_bros_env.make("SuperMarioBros-v0", render_mode=None)
 env = JoypadSpace(env, SIMPLE_MOVEMENT)
 
-env = wrappers.GrayscaleEnv(env)
-env = wrappers.DownscaledEnv(env, 16)
-env = wrappers.DownsampledEnv(env, 8)
+env = GrayscaleEnv(env, GrayscaleFilters.BLUE)
+env = DownscaledEnv(env, 16)
+env = DownsampledEnv(env, 8)
 
 env = DummyVecEnv([lambda: env])
 env = VecFrameStack(env, 4, channels_order="last")
