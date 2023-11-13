759cb40d353f001df3707b10f9e060275a29b449
Katarzyna Szlejter <kasia.szlejter@gmail.com>
2023-11-13 13:40:58 +0100

    Added support for command line arguments: "train" and "run"

--- src/ppo.py
+++ src/ppo.py
@@ -5,7 +5,11 @@ from stable_baselines3 import PPO
 from stable_baselines3.common.vec_env import DummyVecEnv, VecFrameStack
 from wrappers import GrayscaleEnv, DownsampledEnv, DownscaledEnv
 from wrappers.filters import GrayscaleFilters
+import sys
+
 # tjuvlånt fra https://github.com/BJEnrik/reinforcement-learning-super-mario
+
+# Use "python ppo.py train" or "python ppo.py run"
 
 env = mario_bros_env.make("SuperMarioBros-v0", render_mode="human")
 env = JoypadSpace(env, SIMPLE_MOVEMENT)
@@ -27,16 +31,27 @@ except BaseException:
      print("Failed to load existing model, creating new")
      model = PPO("MlpPolicy", env, verbose=True)
 
+
 try:
-     model = model.learn(total_timesteps=1024 * 1024, progress_bar=True)
-except KeyboardInterrupt:
-     print("Saving and quitting...")
-     model.save("models/PPO.zip")
-     print("Saved :)")
+     command = sys.argv[1].lower()
+except IndexError:
+     print("Use command \"train\" or \"run\".")
      exit(1)
 
-state = env.reset()
+if command == "train":
+     try:
+          model = model.learn(total_timesteps=1024 * 1024, progress_bar=True)
+     except KeyboardInterrupt:
+          print("Saving and quitting...")
+          model.save("models/PPO.zip")
+          print("Saved :)")
+          exit(1)
+elif command == "run":
+     state = env.reset()
+     while True:
+          action, _ = model.predict(state)
+          state, reward, done, info = env.step(action)
+     
 
-while True:
-    action, _ = model.predict(state)
-    state, reward, done, info = env.step(action)
+
+
