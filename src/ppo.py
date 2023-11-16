34559366600380e1c3b57268524f0ba491d5061e
Jakob Grønhaug <jakob@gronha.ug>
2023-11-16 10:39:24 +0100

    lint

--- src/ppo.py
+++ src/ppo.py
@@ -7,8 +7,6 @@ from stable_baselines3.common.vec_env import DummyVecEnv, VecFrameStack
 from wrappers import GrayscaleEnv, DownsampledEnv, DownscaledEnv
 from wrappers.filters import GrayscaleFilters
 import sys
-from datetime import datetime
-import os
 
 
 # Use "python ppo.py train" or "python ppo.py run".
@@ -28,46 +26,43 @@ env = VecFrameStack(env, 8, channels_order="last")
 
 model: PPO
 
+
 # Save model to zip file
 def saveModel(model):
-     print("Saving and quitting...")
-     model.save("models/PPO.zip")
-     print("Saved :)")
-     exit(1)
+    print("Saving and quitting...")
+    model.save("models/PPO.zip")
+    print("Saved :)")
+    exit(1)
 
 
 # Load existing model or create new one if there is none
 try:
-     print("Trying to load stored pre-trained model...")
-     model = PPO.load("models/PPO.zip", env)
-     print("Loaded! :)")
-     exit()
+    print("Trying to load stored pre-trained model...")
+    model = PPO.load("models/PPO.zip", env)
+    print("Loaded! :)")
+    exit()
 except BaseException:
-     print("Failed to load existing model, creating new")
-     model = PPO("MlpPolicy", env, verbose=True)
+    print("Failed to load existing model, creating new")
+    model = PPO("MlpPolicy", env, verbose=True)
 
 
 # Read command line arguments
 try:
-     command = sys.argv[1].lower()
+    command = sys.argv[1].lower()
 except IndexError:
-     print("Use command \"train\" or \"run\".")
-     exit(1)
+    print('Use command "train" or "run".')
+    exit(1)
 
 # Train
 if command == "train":
-     try:
-          model = model.learn(total_timesteps=1024 * 1024, progress_bar=True)
-          saveModel(model)
-     except KeyboardInterrupt:
-          saveModel(model)
+    try:
+        model = model.learn(total_timesteps=1024 * 1024, progress_bar=True)
+        saveModel(model)
+    except KeyboardInterrupt:
+        saveModel(model)
 # Run
 elif command == "run":
-     state = env.reset()
-     while True:
-          action, _ = model.predict(state)
-          state, reward, done, info = env.step(action)
-     
-
-
-
+    state = env.reset()
+    while True:
+        action, _ = model.predict(state)
+        state, reward, done, info = env.step(action)
