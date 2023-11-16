c43f257be488141414dbb6d86b2f55610bdeb9c1
Jakob Grønhaug <jakob@gronha.ug>
2023-11-16 11:00:11 +0100

    🚧 Add WIP filter illustration script

--- src/demo_resize_gray.py
+++ src/demo_resize_gray.py
@@ -3,23 +3,44 @@ import mario_bros_env
 from mario_bros_env.actions import RIGHT_ONLY
 from matplotlib import pyplot as plt
 
-from wrappers import DownsampledEnv, DownscaledEnv, GrayscaleEnv
-from wrappers.filters import GrayscaleFilters
+import wrappers.filters
 
 env = mario_bros_env.make("SuperMarioBros-v0", render_mode=None)
 env = JoypadSpace(env, RIGHT_ONLY)
-env = GrayscaleEnv(env, GrayscaleFilters.RED)
-env = DownscaledEnv(env, 16)
-env = DownsampledEnv(env, 3)
 
 state, info = env.reset()
 
-for _ in range(200):
+for _ in range(500):
     state, _, _, _, _ = env.step(env.action_space.sample())
 
 fig = plt.figure()
 
+plt.subplot(2, 2, 1)
+plt.title("Original frame")
+plt.axis('off')
 plt.imshow(state, cmap="Greys_r")
+
+state = wrappers.filters.red_channel(state)
+
+plt.subplot(2, 2, 2)
+plt.title("Red channel only")
+plt.axis('off')
+plt.imshow(state, cmap="Greys_r")
+
+state = wrappers.filters.downscale.divide(state, 12)
+
+plt.subplot(2, 2, 3)
+plt.title("Downscaled")
+plt.axis('off')
+plt.imshow(state, cmap="Greys_r")
+
+state = wrappers.filters.downsample(state, 6)
+
+plt.subplot(2, 2, 4)
+plt.title("Downsampled")
+plt.axis('off')
+plt.imshow(state, cmap="Greys_r")
+
 plt.show()
 
 env.close()
